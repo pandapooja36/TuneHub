cbc369b1b9c354dfcd6fd35aa3ccec7e97033816
Pooja Panda <143191741+pandapooja36@users.noreply.github.com>
2024-05-10 12:05:10 +0530

    Add files via upload

---
+++ ProjectTuneHub/src/main/java/com/kodnest/tunehub/controller/NavController.java
@@ -0,0 +1,29 @@
+package com.kodnest.tunehub.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin("*")
+@RestController
+public class NavController {
+
+	@GetMapping("/login")
+	public String login(){
+		return "login";
+	}
+
+	@GetMapping("/registration")
+	public String registration(){
+		return "registration";
+	}
+
+	@GetMapping("/newsong")
+	public String newsong() {
+		return "newsong";
+	}
+}
+
+
+
+
